54810b1b0325e8098fbafa3d0797a1fcaeef73b9
MrBRANDONe <122766763+MrBRANDONe@users.noreply.github.com>
2023-01-16 18:26:07 -0500

    Code Java

---
+++ introJava/VariableTuto.java
@@ -0,0 +1,83 @@
+public class VariableTuto{
+        public static void main(String []args){
+        	/*String firstName = "serge";
+        	String lastName = "kabwizi";
+        	int age = 27;
+            boolean isActive = false;
+        	
+            if(age > 25) {
+            	System.out.println("est mpaka");
+            }else if(age > 18) {
+            	System.out.println("est majeur");
+            }
+            else {
+            	System.out.println("est mineur");
+            }
+        	
+        	
+        	*/
+        	
+        	
+        	String recitation = "Maître corbeau, sur un arbre perché,\r\n"
+        			+ "Tenait en son bec un fromage.\r\n"
+        			+ "Maître renard, par l’odeur alléché,\r\n"
+        			+ "Lui tint à peu près ce langage :\r\n"
+        			+ "« Hé ! bonjour, Monsieur du Corbeau.\r\n"
+        			+ "Que vous êtes joli ! que vous me semblez beau !\r\n"
+        			+ "Sans mentir, si votre ramage\r\n"
+        			+ "Se rapporte à votre plumage,\r\n"
+        			+ "Vous êtes le phénix des hôtes de ces bois. »\r\n"
+        			+ "À ces mots, le corbeau ne se sent pas de joie ;\r\n"
+        			+ "Et pour montrer sa belle voix,\r\n"
+        			+ "Il ouvre un large bec, laisse tomber sa proie.\r\n"
+        			+ "Le renard s’en saisit, et dit : « Mon bon monsieur,\r\n"
+        			+ "Apprenez que tout flatteur\r\n"
+        			+ "Vit aux dépens de celui qui l’écoute.\r\n"
+        			+ "Cette leçon vaut bien un fromage sans doute. »\r\n"
+        			+ "Le corbeau honteux et confus,\r\n"
+        			+ "Jura, mais un peu tard, qu’on ne l’y prendrait plus.";
+        	
+        
+        	System.out.println("premier: "+ recitation);
+        	System.out.println("deuxieme: " + recitation);
+         	System.out.println("la maison de mon pere: " + recitation);
+        	
+        	/* premier:  Maître corbeau, sur un arbre perché,
+						Tenait en son bec un fromage.
+						Maître renard, par l’odeur alléché,
+						Lui tint à peu près ce langage :
+						« Hé ! bonjour, Monsieur du Corbeau.
+						Que vous êtes joli ! que vous me semblez beau !
+						Sans mentir, si votre ramage
+						Se rapporte à votre plumage,
+						Vous êtes le phénix des hôtes de ces bois. »
+						À ces mots, le corbeau ne se sent pas de joie ;
+						Et pour montrer sa belle voix,
+						Il ouvre un large bec, laisse tomber sa proie.
+						Le renard s’en saisit, et dit : « Mon bon monsieur,
+						Apprenez que tout flatteur
+						Vit aux dépens de celui qui l’écoute.
+						Cette leçon vaut bien un fromage sans doute. »
+						Le corbeau honteux et confus,
+						Jura, mais un peu tard, qu’on ne l’y prendrait plus.*/
+        	
+        	/* deuxieme:  Maître corbeau, sur un arbre perché,
+							Tenait en son bec un fromage.
+							Maître renard, par l’odeur alléché,
+							Lui tint à peu près ce langage :
+							« Hé ! bonjour, Monsieur du Corbeau.
+							Que vous êtes joli ! que vous me semblez beau !
+							Sans mentir, si votre ramage
+							Se rapporte à votre plumage,
+							Vous êtes le phénix des hôtes de ces bois. »
+							À ces mots, le corbeau ne se sent pas de joie ;
+							Et pour montrer sa belle voix,
+							Il ouvre un large bec, laisse tomber sa proie.
+							Le renard s’en saisit, et dit : « Mon bon monsieur,
+							Apprenez que tout flatteur
+							Vit aux dépens de celui qui l’écoute.
+							Cette leçon vaut bien un fromage sans doute. »
+							Le corbeau honteux et confus,
+							Jura, mais un peu tard, qu’on ne l’y prendrait plus.*/
+        }
+}
